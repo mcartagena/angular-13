3048fddac34fc2321c339c2753801e461424dc51
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2023-02-10 07:31:38 -0300

    Manipulating the DOM through renderer2 directive

---
+++ custom-directives/src/app/CustomDirective/highlight.directive.ts
@@ -0,0 +1,18 @@
+import { OnInit } from '@angular/core';
+import { ElementRef, Renderer2 } from '@angular/core';
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective implements OnInit {
+
+  constructor(private element: ElementRef, private renderer: Renderer2) {
+
+   }
+  ngOnInit(): void {
+    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor','#F1948A');
+    this.renderer.addClass(this.element.nativeElement, 'container');
+    this.renderer.setAttribute(this.element.nativeElement, 'title', 'This is example div');
+  }
+}
--- custom-directives/src/app/app.module.ts
+++ custom-directives/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './CustomDirective/highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SetBackgroundDirective
+    SetBackgroundDirective,
+    HighlightDirective
   ],
   imports: [
     BrowserModule
