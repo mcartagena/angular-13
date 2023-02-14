13a04d5107a8ff38200bbe7a48cfa7d5c56429ab
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2023-02-14 07:53:18 -0300

    Adding @HostListner directive example

--- custom-directives/src/app/app.module.ts
+++ custom-directives/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './CustomDirective/highlight.directive';
+import { HoverDirective } from './CustomDirective/hover.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     SetBackgroundDirective,
-    HighlightDirective
+    HighlightDirective,
+    HoverDirective
   ],
   imports: [
     BrowserModule
