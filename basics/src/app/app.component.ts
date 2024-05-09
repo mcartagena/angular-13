09662921d25eb95433d8a1b35103f5772687cc61
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-05-09 16:45:26 -0400

    fea: adding interpolation property binding event binding type assertion

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -10,4 +10,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'basics';
+  name = 'Marcelo';
+  imgURL = 'https://fastly.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0'
+
+  getName(){
+    return this.name;
+  }
+
+  changeImage(e: KeyboardEvent){
+    this.imgURL = (e.target as HTMLInputElement).value;
+  }
 }
