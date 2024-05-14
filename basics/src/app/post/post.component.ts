84ed9e42b896c4f9d680b08e3213b8f88e7420d9
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-05-14 10:00:39 -0400

    fea: adding component and input decorator

---
+++ basics/src/app/post/post.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './post.component.html',
+  styleUrl: './post.component.css'
+})
+export class PostComponent {
+
+  @Input() postImg = '';
+}
