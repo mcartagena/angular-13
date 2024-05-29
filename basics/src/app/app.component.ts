46c3ecfac07dec24065ec5fef3a50ef2d7d4f8a1
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-05-29 10:34:07 -0400

    fea: adding pipes

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -1,20 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostComponent } from './post/post.component';
+import { TitleCasePipe, DatePipe,
+  CurrencyPipe, DecimalPipe, JsonPipe } from "@angular/common"
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    PostComponent
+    PostComponent,
+    TitleCasePipe,
+    DatePipe,
+    CurrencyPipe,
+    DecimalPipe,
+    JsonPipe
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'basics';
-  name = 'Marcelo';
-  imgURL = 'https://fastly.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0'
+  name = 'marcelo cartagena';
+  imgURL = 'https://fastly.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0';
+  currentDate = new Date();
+  cost = 2000;
+  temperature = 25.3;
+  pizza = {
+    toppings: ['pepperoni', 'bacon'],
+    size: 'large'
+  };
 
   getName(){
     return this.name;
