5a8ea1a5681d37355fc0120f7de240feb53fb938
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-07-15 15:21:18 -0400

    fea: validating emails numbers passwords with patterns and input masking

--- clips/src/app/shared/input/input.component.ts
+++ clips/src/app/shared/input/input.component.ts
@@ -1,17 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { NgIf } from '@angular/common';
+import { NgxMaskDirective } from 'ngx-mask';
 
 @Component({
   selector: 'app-input',
   standalone: true,
   imports: [ ReactiveFormsModule,
-    NgIf
+    NgIf,
+    NgxMaskDirective
    ],
   templateUrl: './input.component.html',
-  styleUrl: './input.component.css'
+  styleUrl: './input.component.css',
 })
 export class InputComponent {
   @Input() control: FormControl = new FormControl()
+  @Input() type = 'text'
+  @Input() placehoder = ''
+  @Input() format = ''
 
 }
--- clips/src/app/user/register/register.component.ts
+++ clips/src/app/user/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { JsonPipe, NgIf } from '@angular/common';
 import { InputComponent } from '../../shared/input/input.component';
+import { AlertComponent } from '../../shared/alert/alert.component';
 
 @Component({
   selector: 'app-register',
@@ -9,7 +10,8 @@ import { InputComponent } from '../../shared/input/input.component';
   imports: [ReactiveFormsModule,
     JsonPipe,
     NgIf,
-    InputComponent
+    InputComponent,
+    AlertComponent
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
@@ -20,11 +22,30 @@ export class RegisterComponent implements OnInit {
     Validators.required,
     Validators.minLength(3)
   ])
-  email = new FormControl('')
-  age = new FormControl('')
-  password = new FormControl('')
-  confirm_password = new FormControl('')
-  phoneNumber = new FormControl('')
+  email = new FormControl('', [Validators.required,
+    Validators.email
+  ])
+  age = new FormControl('',[
+    Validators.required,
+    Validators.min(18),
+    Validators.max(120)
+  ]
+  )
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ])
+  confirm_password = new FormControl('',[
+    Validators.required
+  ])
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.minLength(13),
+    Validators.maxLength(13)
+  ])
+  showAlert = false
+  alertMsg = 'Please wait! Your account is being created.'
+  alertColor = 'blue'
 
   ngOnInit(): void {
     this.registerForm = new FormGroup({
@@ -35,5 +56,12 @@ export class RegisterComponent implements OnInit {
       confirm_password: this.confirm_password,
       phoneNumber: this.phoneNumber
     });
+  }
+
+  register():void{
+    this.showAlert = true
+    this.alertMsg = 'Please wait! Your account is being created.'
+    this.alertColor = 'blue'
+
   }
 }
