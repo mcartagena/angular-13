5f4e49430bb847d724b0f92a56e4361f77b3dbb5
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-07-16 16:12:11 -0400

    fea: adding template forms for login

--- clips/src/app/user/login/login.component.ts
+++ clips/src/app/user/login/login.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule,
+    NgIf
+   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  credentials = {
+    email: '',
+    password: ''
+  }
 
+  login(){
+    console.log(this.credentials)
+  }
 }
