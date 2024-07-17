e9920ffa528530bf45dd074b8f82a55462f2694c
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-07-17 11:12:22 -0400

    fea: declaring observable with operator interval

--- rxjs-fundamentals/src/index.js
+++ rxjs-fundamentals/src/index.js
@@ -1,28 +1,7 @@
-import { Observable } from "rxjs";
+import { interval } from "rxjs";
 
-const observable = new Observable((subscriber)=> {
-    const id = setInterval(()=> {
-        subscriber.next('test')
-        console.log('leak')
-    }, 1000)
+const observable = interval(1000);
 
-    return () => {
-        clearInterval(id)
-    }
-})
-
-const subscription = observable.subscribe({
-    next: (value) => {
-        console.log(value)
-    },
-    complete: () => {
-        console.log('complete called!')
-    },
-    error: (err) => {
-        console.error(err)
-    }
-})
-
-setTimeout(() => {
-    subscription.unsubscribe()
-}, 4000);
+const subscription = observable.subscribe(
+    console.log
+)
