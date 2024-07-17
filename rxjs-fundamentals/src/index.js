effc8418b7c13b17ece4685844d94d55c1a0cb4e
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-07-17 12:35:39 -0400

    fea: working with mergeMap operator rxjs

--- rxjs-fundamentals/src/index.js
+++ rxjs-fundamentals/src/index.js
@@ -1,22 +1,18 @@
 import { fromEvent } from "rxjs";
-import { map } from "rxjs/operators";
+import { mergeMap } from "rxjs/operators";
 import { ajax } from "rxjs/ajax";
 
 const button = document.querySelector("#btn");
 
 const observable = fromEvent(button, "click").pipe(
-  map(() => {
+    mergeMap(() => {
     return ajax.getJSON("https://jsonplaceholder.typicode.com/todos/1");
   })
 );
 
 const subscription = observable.subscribe({
   next(value) {
-    value.subscribe({
-      next(value) {
         console.log(value);
-      },
-    });
   },
   complete() {
     console.log("complete");
