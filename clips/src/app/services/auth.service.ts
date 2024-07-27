4d877878525292a947cbf604229a6cc9c38a7ebe
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-07-27 07:03:06 -0400

    fea: adding IUser type to users collection

--- clips/src/app/services/auth.service.ts
+++ clips/src/app/services/auth.service.ts
@@ -1,29 +1,43 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private usersCollection: AngularFirestoreCollection<IUser>
 
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFirestore
-  ) { }
+  ) {
+    this.usersCollection = db.collection('users')
+  }
 
   public async createUser(userData: IUser){
+    if(!userData.password){
+      throw new Error('Password not provided!')
+    }
     const userCredentials = await this.auth.createUserWithEmailAndPassword(
-      userData.email as string,
-      userData.password as string
+      userData.email,
+      userData.password
     );
 
-    await this.db.collection('users').add({
+    if(!userCredentials.user){
+      throw new Error("User can't be found")
+    }
+
+    await this.usersCollection.doc(userCredentials.user.uid).set({
       name: userData.name,
       email: userData.email,
       age: userData.age,
       phoneNumber: userData.phoneNumber
+    })
+
+    await userCredentials.user.updateProfile({
+      displayName: userData.name
     })
   }
 }
