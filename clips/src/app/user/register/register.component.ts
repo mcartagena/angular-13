9c37affe68f6a973002a336873f792d5f5f39c33
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-07-29 13:14:59 -0400

    fea: validating unique emails - asynchronous validator

--- clips/src/app/user/register/register.component.ts
+++ clips/src/app/user/register/register.component.ts
@@ -11,6 +11,7 @@ import { AlertComponent } from '../../shared/alert/alert.component';
 import { AuthService } from '../../services/auth.service';
 import IUser from '../../models/user.model';
 import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
 
 @Component({
   selector: 'app-register',
@@ -28,7 +29,11 @@ import { RegisterValidators } from '../validators/register-validators';
 export class RegisterComponent implements OnInit {
   registerForm!: FormGroup;
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
-  email = new FormControl('', [Validators.required, Validators.email]);
+  email = new FormControl(
+    '',
+    [Validators.required, Validators.email],
+    [this.emailTaken.validate]
+  );
   age = new FormControl<number | null>(null, [
     Validators.required,
     Validators.min(18),
@@ -49,19 +54,20 @@ export class RegisterComponent implements OnInit {
   alertColor = 'blue';
   inSubmission = false;
 
-  constructor(
-    private auth: AuthService
-  ) {}
+  constructor(private auth: AuthService, private emailTaken: EmailTaken) {}
 
   ngOnInit(): void {
-    this.registerForm = new FormGroup({
-      name: this.name,
-      email: this.email,
-      age: this.age,
-      password: this.password,
-      confirm_password: this.confirm_password,
-      phoneNumber: this.phoneNumber,
-    }, [RegisterValidators.match('password','confirm_password')]);
+    this.registerForm = new FormGroup(
+      {
+        name: this.name,
+        email: this.email,
+        age: this.age,
+        password: this.password,
+        confirm_password: this.confirm_password,
+        phoneNumber: this.phoneNumber,
+      },
+      [RegisterValidators.match('password', 'confirm_password')]
+    );
   }
 
   async register() {
@@ -71,17 +77,16 @@ export class RegisterComponent implements OnInit {
     this.inSubmission = true;
 
     try {
-      await this.auth.createUser(this.registerForm.value as IUser)
-
+      await this.auth.createUser(this.registerForm.value as IUser);
     } catch (error) {
-      console.error(error)
-      this.alertMsg = 'An unexpected error occurred. Please try again later'
-      this.alertColor = 'red'
-      this.inSubmission = false
-      return
+      console.error(error);
+      this.alertMsg = 'An unexpected error occurred. Please try again later';
+      this.alertColor = 'red';
+      this.inSubmission = false;
+      return;
     }
 
-    this.alertMsg = 'Success! Your account has been created.'
-    this.alertColor = 'green'
+    this.alertMsg = 'Success! Your account has been created.';
+    this.alertColor = 'green';
   }
 }
