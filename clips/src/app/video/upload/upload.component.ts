ccfe2dc2dbbda43a290cdc31b7017e6711a4644a
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-08-05 07:53:12 -0400

    fea: Handling files- handling drag and drop events - multi step form

--- clips/src/app/video/upload/upload.component.ts
+++ clips/src/app/video/upload/upload.component.ts
@@ -1,12 +1,34 @@
+import { EventBlockerDirective } from './../../shared/directives/event-blocker.directive';
 import { Component } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-upload',
   standalone: true,
-  imports: [],
+  imports: [
+    EventBlockerDirective,
+    NgClass,
+    NgIf,
+  ],
   templateUrl: './upload.component.html',
   styleUrl: './upload.component.css'
 })
 export class UploadComponent {
+  isDragOver = false
+  nextStep = false
+  file: File | null = null
+
+  storeFile($event: Event){
+    this.isDragOver = false
+
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+
+    if(!this.file || this.file.type !== 'video/mp4'){
+      return
+    }
+
+    this.nextStep = true
+
+  }
 
 }
