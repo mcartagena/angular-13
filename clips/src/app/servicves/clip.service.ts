a4e3aef3bcc2f4f85402fddfe08eb015561e4871
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-08-10 18:00:13 -0400

    fea: deleting clip from storage/database

--- clips/src/app/servicves/clip.service.ts
+++ clips/src/app/servicves/clip.service.ts
@@ -9,6 +9,7 @@ import IClip from '../models/clip.model';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { switchMap, map } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Injectable({
   providedIn: 'root',
@@ -16,8 +17,10 @@ import { of } from 'rxjs';
 export class ClipService {
   public clipsCollection: AngularFirestoreCollection<IClip>;
 
-  constructor(private db: AngularFirestore,
-    private auth: AngularFireAuth
+  constructor(
+    private db: AngularFirestore,
+    private auth: AngularFireAuth,
+    private storage: AngularFireStorage
   ) {
     this.clipsCollection = db.collection('clips');
   }
@@ -47,5 +50,17 @@ export class ClipService {
     return this.clipsCollection.doc(id).update({
       title
     })
+  }
+
+  async deleteClip(clip: IClip){
+
+    console.log(`deleting clips/${clip.fileName}`)
+
+    const clipRef = this.storage.ref(`clips/${clip.fileName}`)
+
+    await clipRef.delete()
+
+    await this.clipsCollection.doc(clip.docID).delete()
+
   }
 }
