369da748b18bc5fae525c3f012a67cca84c40200
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-08-10 18:41:49 -0400

    fea: sorting clips with behavior subjects

--- clips/src/app/video/manage/manage.component.ts
+++ clips/src/app/video/manage/manage.component.ts
@@ -5,6 +5,7 @@ import IClip from '../../models/clip.model';
 import { NgFor } from '@angular/common';
 import { EditComponent } from '../edit/edit.component';
 import { ModalService } from '../../services/modal.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-manage',
@@ -22,6 +23,7 @@ export class ManageComponent implements OnInit {
   videoOrder = '1'
   clips: IClip[] = []
   activeClip: IClip | null = null
+  sort$: BehaviorSubject<string>
 
   constructor(
     private router: Router,
@@ -29,13 +31,14 @@ export class ManageComponent implements OnInit {
     private clipService: ClipService,
     private modal: ModalService
   ){
-
+    this.sort$ = new BehaviorSubject(this.videoOrder)
   }
   ngOnInit(): void {
     this.route.queryParams.subscribe((params: Params) => {
       this.videoOrder = params['sort'] === '2'? params['sort'] : '1'
+      this.sort$.next(this.videoOrder)
     })
-    this.clipService.getUserClips().subscribe(docs =>{
+    this.clipService.getUserClips(this.sort$).subscribe(docs =>{
       this.clips = []
       docs.forEach(doc => {
         this.clips.push({
