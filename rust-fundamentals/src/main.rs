cd81c80e59f9cc87f884e5d3164d65f326f08cb5
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-08-12 09:47:44 -0400

    fea: variables - data types - macros - control flow

--- rust-fundamentals/src/main.rs
+++ rust-fundamentals/src/main.rs
@@ -1,3 +1,22 @@
+fn add(num_one: i32, num_two: i32) -> i32 {
+    num_one + num_two
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut  my_name = "Marcelo";
+    my_name = "Marcelo Fernando";
+
+    let total = add(10, 20);
+
+    if total > 50 {
+        print!("You quality for free shipping!");
+    }
+    else if total > 20 {
+        print!("if you add more items, you can qualify for free shipping")
+    }
+    else {
+        print!("No free shipping")
+    }
+
+    print!("{:?} {}", total, my_name);
 }
