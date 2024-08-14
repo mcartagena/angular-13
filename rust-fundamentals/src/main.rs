52e4f7cff98a6342dd8632a8259bc58463987fd7
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-08-14 05:45:27 -0400

    fea: rust vectors

--- rust-fundamentals/src/main.rs
+++ rust-fundamentals/src/main.rs
@@ -6,17 +6,44 @@ fn main() {
     let mut  my_name = "Marcelo";
     my_name = "Marcelo Fernando";
 
-    let total = add(10, 20);
+    let mut total = add(40, 20);
+    let mut free_shipping = false;
 
     if total > 50 {
-        print!("You quality for free shipping!");
+        println!("You quality for free shipping!");
+        free_shipping = true
     }
     else if total > 20 {
-        print!("if you add more items, you can qualify for free shipping")
+        println!("if you add more items, you can qualify for free shipping")
     }
     else {
         print!("No free shipping")
     }
 
-    print!("{:?} {}", total, my_name);
+    total = match free_shipping {
+        true =>  total + 0,
+        false => total + 5  
+    };
+
+    match total {
+        1 => println!("match 1"),
+        2 => println!("match 2"),
+        _ => println!("there isn't match")
+    };
+
+    println!("{:?} {}", total, my_name);
+
+    let items: [i32; 5] = [1,2,3,4,5];
+    println!("{:?}", items);
+
+    let vector_items = vec![1,2,3,4,5];
+    let mut vector_items_2 = Vec::new();
+    vector_items_2.push(1);
+    vector_items_2.push(2);
+    vector_items_2.push(3);
+    vector_items_2.push(4);
+    vector_items_2.push(5);
+
+    println!("{:?}",vector_items);
+    println!("{:?}",vector_items_2);
 }
