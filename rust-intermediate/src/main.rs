11367e2e44ca76fe92fbed8e7bc59d62642ac3be
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-08-14 06:02:11 -0400

    fea: rust ownership

--- rust-intermediate/src/main.rs
+++ rust-intermediate/src/main.rs
@@ -3,12 +3,20 @@ struct BankAccount {
     verified: bool
 }
 
+fn print_balance(account: &BankAccount){
+    println!("{:?}", account.balance);
+}
+
+fn print_verified(account: &BankAccount){
+    println!("{:?}", account.verified);
+}
+
 fn main() {
     let my_account = BankAccount {
         balance: 20,
         verified: true
     };
 
-    println!("{:?}", my_account.balance);
-    println!("{:?}", my_account.verified);
+    print_balance(&my_account);
+    print_verified(&my_account);
 }
