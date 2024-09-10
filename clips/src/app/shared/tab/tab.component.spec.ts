47cf540e0c81f75101f4e8d36d3e08f35cd3f36f
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-09-10 11:20:40 -0300

    fea: testing nested components and testing content projection

--- clips/src/app/shared/tab/tab.component.spec.ts
+++ clips/src/app/shared/tab/tab.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { TabComponent } from './tab.component';
 
@@ -8,10 +9,9 @@ describe('TabComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TabComponent]
-    })
-    .compileComponents();
-    
+      imports: [TabComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(TabComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,22 @@ describe('TabComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have .hidden class', () => {
+    const element = fixture.debugElement.query(By.css('.hidden'));
+    const element2 = fixture.nativeElement.querySelector('.hidden');
+    const element3 = document.querySelector('.hidden');
+
+    expect(element).toBeTruthy();
+  });
+
+  it('should not have .hidden class', () => {
+    component.active = true;
+    fixture.detectChanges();
+
+    const element = fixture.debugElement.query(By.css('.hidden'));
+
+    expect(element).not.toBeTruthy();
   });
 });
