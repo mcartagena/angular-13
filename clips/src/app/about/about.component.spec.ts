9287c36267022ea758ff731c71ed9332d9a9f993
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-09-10 10:17:06 -0300

    fea: testing components instance - querying component template - inverting matchers

--- clips/src/app/about/about.component.spec.ts
+++ clips/src/app/about/about.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
 
-describe('AboutComponent', () => {
+describe('About Component', () => {
   let component: AboutComponent;
   let fixture: ComponentFixture<AboutComponent>;
 
@@ -11,11 +11,15 @@ describe('AboutComponent', () => {
       imports: [AboutComponent]
     })
     .compileComponents();
-    
+
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
